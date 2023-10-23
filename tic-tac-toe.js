bfad2f08539636591faa50b517655a04b0a9c545
JZG <jzg924@gmail.com>
2023-10-22 23:00:20 -0500

    Added Web

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,36 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all the squares in the 3x3 grid
+    
     const squares = document.querySelectorAll(".square");
-
-    // Initialize variables
-    let currentPlayer = "X"; // X starts the game
-    let gameState = ["", "", "", "", "", "", "", "", ""]; // Empty game state
-
-    // Add a click event listener to each square
+    const button = document.getElementById("button");
+    const status = document.getElementById("status");
+    
+    let Player = "X";
+    let gameState = ["", "", "", "", "", "", "", "", ""]; 
+    let gameEnd= false;
+    
+    
     squares.forEach(function(square, index) {
-        // Add the "square" class to each square
+        
         square.classList.add("square");
 
         square.addEventListener("click", function() {
-            // Check if the square is empty and the game is still ongoing
-            if (!gameState[index] && !isGameWon()) {
-                // Update the game state and square content
-                gameState[index] = currentPlayer;
-                square.textContent = currentPlayer;
+        
+            if (!gameState[index+1] && !isGameWon()) {
+                
+                gameState[index+1] = Player;
+                square.textContent = Player;
 
-                // Add the class "X" or "O" for styling
-                square.classList.add(currentPlayer);
+                square.classList.add(Player);
 
-                // Toggle the current player
-                currentPlayer = currentPlayer === "X" ? "O" : "X";
+                
+                Player = Player === "X" ? "O" : "X";
             }
+            if(isGameWon()){
+                Player=Player === "X" ? "O" : "X";
+                win=Player;
+                status.textContent="Congratulations!"+" "+win+" "+"is the Winner";
+                status.classList.add("you-won");
+            }
+            
+            
         });
     });
+    
+    button.addEventListener("click", function() {
+        
+        squares.forEach(square => {
+            square.textContent = "";
+            square.classList.remove("X", "O");
+        });
+        gameState = ["", "", "", "", "", "", "", "", ""];
+        Player = "X";
 
-    // Function to check if the game is won (You need to implement this)
+        
+        
+        status.textContent = "Move your mouse over a square and click to play an X or an O.";
+        status.classList.remove("you-won");
+    
+    })
+        
+    
     function isGameWon() {
-        // Implement your win condition logic here
-        // Check rows, columns, and diagonals for a win
-        // Return true if the game is won, otherwise return false
+        const HowtoWin = [
+            [1,2,3], [4,5,6], [7,8,9], 
+            [1,4,7], [2,5,8], [3,6,9],
+            [1,5,9], [3,5,7] 
+        ];
+    
+        
+        for (const combination of HowtoWin) {
+            const [a, b, c] = combination;
+    
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                return true; 
+            }
+        }
+    
+        return false;
     }
-});
+    
+})
